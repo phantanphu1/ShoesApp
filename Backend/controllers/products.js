5d3be9bd50aaf8f01ff884475fb3291834f26037
phantanphu1 <phantanphu1505@gmail.com>
2022-10-19 16:22:05 +0700

    fix API

--- Backend/controllers/products.js
+++ Backend/controllers/products.js
@@ -1,47 +1,45 @@
-const Products = require('../models/products')
+const Products = require("../models/products");
 
-
-// CRUD
-// CREAT - POST
+//CRUD
+//CREATE-POST
 const createProduct = (req, res, next) => {
-    try {
-        const { 
-            productName,
-            ProductBrand,
-            type,
-            price,
-            discount,
-            quantity,
-            images,
-        } = req.body
-        if (
-            !productName ||
-            !ProductBrand ||
-            !type ||
-            !price ||
-            !discount ||
-            !quantity ||
-            !images
-        ) {
-            res.status(400).json({
-                statusCode: 400,
-                message: 'Some fields are not empty.',
-            })
-        }
-        let product = new Products(req.body)
-        // product.save()
-        product.save().then((response) => {
-            res.json({
-              message: 'Added product successfully!',
-            })
-          })
-    } catch (error) {
-        console.log(error);
+  try {
+    const {
+      productName,
+      productBrand,
+      type,
+      price,
+      discount,
+      quantity,
+      images,
+    } = req.body;
+    if (
+      !productName ||
+      !productBrand ||
+      !type ||
+      !price ||
+      !discount ||
+      !quantity ||
+      !images
+    ) {
+      res.status(400).json({
+        statuscode: 400,
+        message: "Some fileds are not empty.",
+      });
     }
-}
+    let product = new Products(req.body);
+    // product.save()
+    product.save().then((response) => {
+      res.json({
+        message: "Added product successfully!",
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// READ - GET||POST
-// UPDATE - PUT ||PATCH
-// DELETE - DELETE
-
-module.exports = { createProduct }
+//READ-GET  || POST
+//UPDATE-PUT||PATCH
+//DELETE-DELETE
+module.exports = { createProduct };
