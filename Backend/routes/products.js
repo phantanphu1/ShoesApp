80a7655fa45061afc3850d646cc6ef1e5a0b5a02
phantanphu1 <phantanphu1505@gmail.com>
2022-11-01 10:41:24 +0700

    fix

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const route = express.Router()
-const app = express()
+const express = require("express");
+const route = express.Router();
+const app = express();
 
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
-    next();
-}
+  next();
+};
 
-const ProductsController = require('../controllers/products')
+const ProductsController = require("../controllers/products");
 
 // app.use(cors({ origin: '*', credentials: true }))
-app.use(allowCrossDomain)
+app.use(allowCrossDomain);
 
-route.post('/products/create', ProductsController.createProduct)
-route.get('/products/getAllProducts', ProductsController.getAllProducts)
-route.get('/products/getProductById/:productId', ProductsController.getProductById)
-route.delete('/products/deleteProductById/:productId', ProductsController.deleteProductById)
-route.patch('/products/editProductById/:productId', ProductsController.editProduct)
+route.post("/products/create", ProductsController.createProduct);
+route.get("/products/getAllProducts", ProductsController.getAllProducts);
+route.get(
+  "/products/getProductById/:productId",
+  ProductsController.getProductById
+);
+route.delete(
+  "/products/deleteProductById/:productId",
+  ProductsController.deleteProductById
+);
+route.patch(
+  "/products/editProductById/:productId",
+  ProductsController.editProduct
+);
 
-module.exports = route
+module.exports = route;
